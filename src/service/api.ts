35e07eb4225dc2ffad649e3dd2d6465a1767bb81
Jvalentim <jvalentim0701@gmail.com>
2025-10-06 17:04:22 -0300

    Set Env url

--- src/service/api.ts
+++ src/service/api.ts
@@ -2,7 +2,11 @@ import axios from "axios";
 import type { AxiosInstance } from "axios"; 
 
 // Config for API URL
-const baseURL: string = "http://localhost:8080"
+const baseURL: string = import.meta.env.VITE_API_URL
+
+if (!baseURL) {
+  console.warn("⚠️ VITE_API_URL não definida!");
+}
 
 // Config for setup base url on axios
 const api: AxiosInstance = axios.create({
